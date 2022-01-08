4a98f9a3864a584e86c615f4aaa17f1e57c72e06
Will Hartman <me@willhartman.co.uk>
2022-01-08 20:54:06 +0000

    feat(javascript-basics): Math Random
    Co-authored-by: Exuro555 <Exuro555@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,14 @@
+let displayName = 'Tom';
+const editorName = 'VISUAL STUDIO CODE';
+
+let numberOfMonitors = 2;
+
+let totalNumberOfMonitors = numberOfMonitors * 5;
+
+let arrayOfNumbers = [1,2,3,4,5];
+
+let num = Math.random();
+
+arrayOfNumbers.push(num);
+
+arrayOfNumbers[0] = Math.random();
